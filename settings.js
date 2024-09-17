2ab71450ef88ed1f195454b14743dd64dd466cad
Tikka2008 <damsarat48@gmail.com>
2024-09-17 13:34:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94761180276' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=6AtyDSZI#LpE3SioJ6i8RZcmVBDPIPGn_WdPGfdohmj2M0tlZceY' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=SAcXyIyD#PHogvXE_bq4bzYrFGNc5mxSlTwwyv1ZMQrw1SEMFqsQ' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
